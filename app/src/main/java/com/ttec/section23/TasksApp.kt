f84c1b0cac269c655262863ad68e902cb973ce3e
José Zambrano <jzambra1@tragsa.es>
2024-03-14 08:46:43 +0100

    created project structure

---
+++ app/src/main/java/com/ttec/section23/TasksApp.kt
@@ -0,0 +1,6 @@
+package com.ttec.section23
+
+import android.app.Application
+
+class TasksApp: Application() {
+}
---
+++ app/src/main/java/com/ttec/section23/addtasks/ui/TasksViewModel.kt
@@ -0,0 +1,6 @@
+package com.ttec.section23.addtasks.ui
+
+import androidx.lifecycle.ViewModel
+
+class TasksViewModel: ViewModel() {
+}
